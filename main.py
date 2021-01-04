8fd6abcb976fb9e8e6aaeb8c0fe2212151cae2cd
Cole Stanfield <cole.stanfield@me.com>
2021-01-04 16:33:51 -0700

    Basic trading bot working with SMA strategy

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import strategy as Strategy
+
 START_DATE = '2019-01-01'
 END_DATE = '2020-12-31'
-
+STOCK = 'fb'
 
 def get_stock_data(symbol):
     stock_df = yf.download(symbol,
-                             start=START_DATE,
-                             END_DATE=END_DATE,
-                             progress=False)
-    stock_df['Close'].plot(title="{} stock price".format(symbol))
+                           start=START_DATE,
+                           END_DATE=END_DATE,
+                           progress=False)
+    return stock_df
+
+def simple_moving_average(data, window):
+    data['SMA_{}'.format(window)] = data['Close'].rolling(window=window).mean()
+    return data
+
+def plot(data, symbol):
+    data['Close'].plot(title="{} stock price".format(symbol))
+    data['SMA_5'].plot()
+    data['SMA_20'].plot()
     plt.show()
-    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_stock_data('aapl')
+    data = get_stock_data(STOCK)
+    data = simple_moving_average(data, window=5)
+    data = simple_moving_average(data, window=20)
+    data = data.dropna()
+
+    strat = Strategy.Strategy(data)
+    strat.backtest()
+    plot(data, STOCK)
+    x = 5
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- strategy.py
+++ strategy.py
@@ -1,2 +1,58 @@
-class strategy:
-    pass
+import pandas as pd
+import math
+
+
+class Strategy:
+    SELL = 0
+    BUY = 1
+    HOLD = 2
+    TREND = -1
+
+    metrics = ['SMA_5', 'SMA_20']
+    cash = 1000
+    shares = 0
+
+    def __init__(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def execute_trade(self, decision, shares, price):
+        if decision == self.BUY:
+            self.cash = self.cash - (shares * price)
+            self.shares = self.shares + shares
+            print("Trade Executed. \tShares: {}\tPrice: {}\tTrade Type: {}\tTotal Value: {}".format(
+                shares, price, "BUY", (self.cash + self.shares * price)
+            ))
+        if decision == self.SELL:
+            self.cash = self.cash + (shares * price)
+            self.shares = self.shares - shares
+            print("Trade Executed. \tShares: {}\tPrice: {}\tTrade Type: {}\tTotal Value: {}".format(
+                shares, price, "SELL", (self.cash + self.shares * price)
+            ))
+        pass
+
+    def decision(self, row):
+        if self.cash <= 0:
+            print("Out of Cash!!!")
+            return
+        if row['SMA_5'] >= row['SMA_20'] and self.TREND == 0:
+            self.TREND = 1
+            shares = math.floor(self.cash / row['Close'])
+            self.execute_trade(self.BUY, shares, row['Close'])
+        if row['SMA_5'] <= row['SMA_20'] and self.TREND == 1:
+            self.TREND = 0
+            self.execute_trade(self.SELL, self.shares, row['Close'])
+        if row['SMA_5'] >= row['SMA_20'] and self.TREND == -1:
+            self.TREND = 1
+            shares = math.floor(self.cash / row['Close'])
+            self.execute_trade(self.BUY, shares, row['Close'])
+
+    def backtest(self):
+        self.data.apply(lambda row: self.decision(row), axis=1)
+        current_price = self.data['Close'][self.data.last_valid_index()]
+        print("ACCOUNT\tShares: {}\tCash: {}\tTotal Value: {}".format(
+            self.shares, self.cash, (self.cash + self.shares * current_price)
+        ))
+        return
