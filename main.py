1909f18c1cef0882a438e9d7785f222b60c73a36
Cole Stanfield <cole.stanfield@me.com>
2021-01-07 17:41:56 -0700

    Completed additional indicators and added simple neural network

--- main.py
+++ main.py
@@ -3,37 +3,34 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import strategy as Strategy
 
-START_DATE = '2019-01-01'
+START_DATE = '2020-12-01'
 END_DATE = '2020-12-31'
-STOCK = 'fb'
+STOCK = 'aapl'
 
 def get_stock_data(symbol):
     stock_df = yf.download(symbol,
                            start=START_DATE,
                            END_DATE=END_DATE,
+                           interval='30m',
                            progress=False)
     return stock_df
 
-def simple_moving_average(data, window):
-    data['SMA_{}'.format(window)] = data['Close'].rolling(window=window).mean()
-    return data
-
 def plot(data, symbol):
-    data['Close'].plot(title="{} stock price".format(symbol))
-    data['SMA_5'].plot()
-    data['SMA_20'].plot()
+    # data['Close'].plot(title="{} stock price".format(symbol))
+    data['On Balance Volume Derivative'].plot()
+    # data['SMA_20'].plot()
+    # data['On Balance Volume'].plot()
+    # data['On Balance Volume Smooth'].plot()
     plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data = get_stock_data(STOCK)
-    data = simple_moving_average(data, window=5)
-    data = simple_moving_average(data, window=20)
-    data = data.dropna()
+    # data = simple_moving_average(data, window=5)
+    # data = simple_moving_average(data, window=20)
+    # data = data.dropna()
+    strategy = Strategy.Strategy(data)
+    strategy.generate_metrics()
+    strategy.generate_neural_net()
+    # plot(data, STOCK)
 
-    strat = Strategy.Strategy(data)
-    strat.backtest()
-    plot(data, STOCK)
-    x = 5
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- strategy.py
+++ strategy.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import numpy as np
 import math
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 
 class Strategy:
@@ -13,7 +19,7 @@ class Strategy:
     shares = 0
 
     def __init__(self, data):
-        self.data = data
+        self.data = pd.DataFrame(data)
 
     def get_data(self):
         return self.data
@@ -56,3 +62,105 @@ class Strategy:
             self.shares, self.cash, (self.cash + self.shares * current_price)
         ))
         return
+
+    def generate_metrics(self):
+        self.rsi_metric()
+        self.macd_metric()
+        self.on_balance_volume()
+        self.simple_moving_average(5)
+        self.simple_moving_average(20)
+        self.exponential_moving_average(5)
+        self.exponential_moving_average(20)
+        self.data['SMA Distance'] = self.data['SMA_5'] - self.data['SMA_20']
+        self.data['EMA Distance'] = self.data['EMA_5'] - self.data['EMA_20']
+        self.data['Increase/Decrease'] = self.data['Close'].rolling(2).apply(lambda x: 1 if x.iloc[1] - x.iloc[0] > 0 else 0)
+        return
+
+    def rsi_metric(self, window=14):
+        # 1: Compute price movement each period
+        # 2: Compute average gain/loss over last 14 days
+        gain = pd.DataFrame(self.data['Close'].rolling(2).apply(lambda x: x.iloc[1] - x.iloc[0]))
+        gain[gain < 0] = np.nan
+        gain = gain.rolling(window=window, min_periods=1).mean()
+        gain = gain.fillna(0)
+
+        loss = pd.DataFrame(self.data['Close'].rolling(2).apply(lambda x: x.iloc[1] - x.iloc[0]))
+        loss[loss > 0] = np.nan
+        loss = loss.abs()
+        loss = loss.rolling(window=window, center=True, min_periods=1).mean()
+        loss = loss.fillna(0)
+        # 3: Calculate RS and RSI
+        relative_strength = gain / loss
+        relative_strength_index = 100 - 100 / (1 + relative_strength)
+        self.data['RSI'] = relative_strength_index
+
+    def macd_metric(self):
+        # 9 period exponential moving average
+        macd_signal = self.data['Close'].ewm(span=9, adjust=False).mean()
+        # 12 period exponential moving average
+        EMA_12_day = self.data['Close'].ewm(span=12, adjust=False).mean()
+        # 26 period exponential moving average
+        EMA_26_day = self.data['Close'].ewm(span=26, adjust=False).mean()
+        macd = EMA_26_day - EMA_12_day
+        self.data['MACD'] = macd
+        self.data['MACD_Signal'] = macd_signal
+        self.data['MACD_Decision'] = macd - macd_signal
+
+    def on_balance_volume(self, span=12):
+        # OBV = Previous OBV + Current Trading Volume
+        self.data['On Balance Volume'] = np.where(self.data['Close'] > self.data['Close'].shift(1), self.data['Volume'], np.where(self.data['Close'] < self.data['Close'].shift(1), -self.data['Volume'], 0)).cumsum()
+        self.data['On Balance Volume Smooth'] = self.data['On Balance Volume'].ewm(span=span, adjust=False).mean()
+        self.data['On Balance Volume Derivative'] = self.data['On Balance Volume Smooth'].diff()
+
+    def simple_moving_average(self, window):
+        self.data['SMA_{}'.format(window)] = self.data['Close'].rolling(window=window).mean()
+
+    def exponential_moving_average(self, span):
+        self.data['EMA_{}'.format(span)] = self.data['Close'].ewm(span=span, adjust=False).mean()
+
+    def generate_neural_net(self):
+        training_data = self.data.dropna()
+        X = training_data[['RSI',
+                           'On Balance Volume Derivative',
+                           'MACD_Decision',
+                           'SMA Distance',
+                           'EMA Distance']]
+        x = X.values
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform((x))
+        X = pd.DataFrame(x_scaled)
+        y = training_data['Increase/Decrease']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        model = Sequential()
+        model.add(Dense(12, input_dim=5, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        # fit the keras model on the dataset
+        history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50, batch_size=10)
+        self.graph_neural_network_results(history)
+        return
+
+    def graph_neural_network_results(self, history):
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.show()
+
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.show()
+
